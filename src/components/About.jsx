675b0be4e86483139d21e87266daaa6caf7be39c
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-11-15 10:48:10 +0300

    Adds an aboutsection

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,13 @@ import React from 'react';
 import './About.css';
 
 const About = () => (
-    <div>
-
+    <div className='about'>
+        <h1 className="about-title">About Data Science</h1>
+        <p className="about-text">
+        Welcome to Data Science, My personal library for tracking and managing 
+        my programming learning journey. This application helps you keep track of 
+        my technical reading list and share reviews with others.
+        </p>
     </div>
   );
 
