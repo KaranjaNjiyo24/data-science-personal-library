918bae8cb3a37934a9617324f0e6582ece5c85d2
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-11-15 10:57:09 +0300

    Adds Book List component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
+import AddBook from './components/AddBook';
+import About from './components/About';
 
-function App() {
+const App = () => {
+  const [books, setBooks] = useState([]);
 
+  useEffect(() => {
+    fetch('http://localhost:3000/books')
+      .then((res) => res.json())
+      .then((data) => setBooks(data));
+  }, []);
+  
   return (
-    <>
+    <Router>
+      <div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/book/:id" element={<BookDetail />} />
+          <Route path="/add" element={<AddBook />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;
